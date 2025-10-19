36f4807f77473322aba48abd86fbad17583be520
Krzysztof Tomicki <tomicki.krzysztof.pv@gmail.com>
2025-10-19 11:23:36 +0200

    Main: add RTC and DI reading

--- Firmware/Firmware/main.c
+++ Firmware/Firmware/main.c
@@ -1,7 +1,7 @@
 /*
  * Firmware.c
  *
- * Created: 29.03.2025 14:58:19
+ * Created: 19.10.2025 14:58:19
  * Author : Krzysztof Tomicki
  */
 
@@ -16,6 +16,52 @@
 #include "rtc.h"
 #include "eeprom.h"
 
-int main(void) {
+#define WINDOW_COUNT 6
+
+const uint8_t window_pins[WINDOW_COUNT] = 
+{
+	PD2, PD3, PD4, PD5, PD6, PD7
+};
+
+typedef struct  
+{
+	uint32_t opens;
+	uint32_t open_secs;
+	uint32_t start_time;
+	uint8_t state;
+} window_data_t;
+
+window_data_t windows[WINDOW_COUNT];
+
+int main(void) 
+{
+	UART_init(103); //9600 baud at 16 MHz
+	I2C_init();
 	
+	uint8_t h, m, s;
+	char buffer[64];
+	
+	for (uint8_t i = 0; i < WINDOW_COUNT; i++)
+	{
+		DDRD &= ~(1 << window_pins[i]); // Set PD2-PD7 pins as inputs
+		PORTD |= (1 << window_pins[i]); // Set pull-ups
+	}
+	
+	while (1)
+	{
+		// Get time from RTC module
+		rtc_get_time(&h, &m, &s);
+		sprintf(buffer, "Time: %02d:%02d:%02d\r\n", h, m, s);
+		UART_send_string(buffer);
+		
+		// Send current windows state
+		for (uint8_t i = 0; i < WINDOW_COUNT; i++)
+		{
+			uint8_t state = !(PIND & (1 << window_pins[i])); // 0 = CLOSED, 1 = OPEN
+			sprintf(buffer, "Window %d: %s\r\n", i + 1, state ? "OPEN" : "CLOSED");
+			UART_send_string(buffer);
+		}
+		
+		_delay_ms(1000);
+	}
 }
